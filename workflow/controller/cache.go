de8ff95d8bf38fb023eb0a1c113185b2dc376585
Remington Breeze <remington@breeze.software>
2020-06-25 17:18:25 -0700

    Refactor cache into two interfaces designed for better mock testing

--- workflow/controller/cache.go
+++ workflow/controller/cache.go
@@ -16,9 +16,14 @@ var sampleEntry = CacheEntry{
 	Outputs: wfv1.Outputs{},
 }
 
+// TWO INTERFACES:
+// Top level of abstraction: Cache for operator to interact with cache
+// Lower level: Interface for interacting with K8S that can be substituted w mock for testing
+
+
 type Cache interface {
-	Load(key []byte) (*wfv1.Outputs, bool)
-	Save(key []byte, value string) bool
+	Load(key string) (*wfv1.Outputs, bool)
+	Save(key string, value *wfv1.Outputs) bool
 }
 
 type CacheEntry struct {
@@ -29,16 +34,33 @@ type CacheEntry struct {
 
 type configMapCache struct {
 	configMapName string
-	kubeClient kubernetes.Interface
-	namespace string
+	configMapClient *configMapClient
 }
 
-func NewConfigMapCache(cm string, ns string, ki kubernetes.Interface) *configMapCache {
-	return &configMapCache{
-		configMapName: cm,
+type ConfigMapClient interface {
+	Create(*apiv1.ConfigMap) (*apiv1.ConfigMap, error)
+	Get(string) (*apiv1.ConfigMap, error)
+	Update(*apiv1.ConfigMap) (*apiv1.ConfigMap, error)
+}
+
+type configMapClient struct {
+	namespace string
+	kubeClient kubernetes.Interface
+}
+
+func NewConfigMapCache(cm string, ns string, ki kubernetes.Interface) Cache {
+	cmc := configMapClient{
 		namespace: ns,
 		kubeClient: ki,
 	}
+	return &configMapCache{
+		configMapName: cm,
+		configMapClient: &cmc,
+	}
+}
+
+func foo(bar ConfigMapClient) {
+	return
 }
 
 func validateCacheKey(key string) string {
@@ -52,21 +74,25 @@ func validateCacheKey(key string) string {
     return s
 }
 
-func (c *configMapCache) Clear() bool {
-	err := c.kubeClient.CoreV1().ConfigMaps(c.namespace).Delete(c.configMapName, &metav1.DeleteOptions{})
+func (c *configMapClient) Create(cm *apiv1.ConfigMap) (*apiv1.ConfigMap, error) {
+	return c.kubeClient.CoreV1().ConfigMaps(c.namespace).Create(cm)
+}
+
+func (c *configMapClient) Get(name string) (*apiv1.ConfigMap, error) {
+	cm, err := c.kubeClient.CoreV1().ConfigMaps(c.namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
-		log.Infof("Error deleting ConfigMap cache %s: %s", c.configMapName, err)
-		return false
+		log.Infof("Error loading ConfigMap cache %s: %s", name, err)
+		return nil, err
 	}
-	return true
+	return cm, nil
+}
+
+func (c *configMapClient) Update(cm *apiv1.ConfigMap) (*apiv1.ConfigMap, error) {
+	return c.kubeClient.CoreV1().ConfigMaps(c.namespace).Update(cm)
 }
 
 func (c *configMapCache) Load(key string) (*wfv1.Outputs, bool) {
-	cm, err := c.kubeClient.CoreV1().ConfigMaps(c.namespace).Get(c.configMapName, metav1.GetOptions{})
-	if err != nil {
-		log.Infof("Error loading ConfigMap cache %s: %s", c.configMapName, err)
-		return nil, false
-	}
+	cm, err := c.configMapClient.Get(c.configMapName)
 	if cm == nil {
 		log.Infof("Cache miss: ConfigMap does not exist")
 		return nil, false
@@ -90,9 +116,9 @@ func (c *configMapCache) Load(key string) (*wfv1.Outputs, bool) {
 
 func (c *configMapCache) Save(key string, value *wfv1.Outputs) bool {
 	log.Infof("Saving to cache %s...", key)
-	cm, err := c.kubeClient.CoreV1().ConfigMaps(c.namespace).Get(c.configMapName, metav1.GetOptions{})
+	cm, err := c.configMapClient.Get(c.configMapName)
 	if len(cm.Data) == 0 && err != nil {
-		_, err = c.kubeClient.CoreV1().ConfigMaps(c.namespace).Create(&apiv1.ConfigMap{
+		_, err = c.configMapClient.Create(&apiv1.ConfigMap{
 				ObjectMeta: metav1.ObjectMeta{
 					Name: c.configMapName,
 				},
@@ -115,7 +141,7 @@ func (c *configMapCache) Save(key string, value *wfv1.Outputs) bool {
 		},
 	}
 
-	_, err = c.kubeClient.CoreV1().ConfigMaps(c.namespace).Update(&opts)
+	_, err = c.configMapClient.Update(&opts)
 
 	if err != nil {
 		log.Infof("Error creating new cache entry for %s: %s", key, err)
